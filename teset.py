4c319ea90d212828854f1eda2149ca1d165df1d1
Moaz hussin <moazhussin45@gmail.com>
2026-06-25 22:18:20 +0300

    Add files via upload

--- teset.py
+++ teset.py
@@ -1,44 +1,74 @@
-#print ("welcome to my application")
-#age = int(input("haw old are you\n"))   
-#if age >= 12:
-    #print(" good you can ues the app") 
-#else:
-      #print("sorry you can,t app")
-#print ("welcome to my lab")
-#score = float(input('How many degrees of closure?\n'))
-#if score >= 100:
-    #print("you got A grade")
-#elif score <=50:
-    #print("you got B grade")
-#elif score >=30:
-    #print("you got C grade")
-#elif score <20:
-    #print("you got f grade")
-#else:
-    #print("no babe")
-#chang_name = int(input("enter your chang number\n"))
-#if chang_name != 30:
-    #print("your win")
-#else:
-    #print("sorry")
-#Password = str(input("type the password\n"))
-#correct_password = "moaz123456"
-#if Password == correct_password:
-    #print("welcome to my phon")
-#else:
-    #print("sorry")
-#typed = str(input( "please typed (yes) , (No) or (maybe)\n"))
-#if typed == "yes":
-    #print(f" you typed {typed}")
-#elif typed == "no":
-    #print(f"you typed {typed}")
-#elif typed == "maybe":
-    #print(f"you typed {typed}")
-#else:
-    #print(f"you typed {typed}, which is not an option \nplease stick to the options")
-#number = int(input("enter number\n"))
-#correct_number = 55
-#if number== correct_number:
-    # print("very good ")
-#else:
-    #print("sorry")
+# print ("welcome to my application")
+# age = int(input("haw old are you\n"))   
+# if age >= 12:
+#     print(" good you can ues the app") 
+# else:
+#       print("sorry you can,t app")
+# print ("welcome to my lab")
+# score = float(input('How many degrees of closure?\n'))
+# if score >= 100:
+#     print("you got A grade")
+# elif score <=50:
+#     print("you got B grade")
+# elif score >=30:
+#     print("you got C grade")
+# elif score <20:
+#     print("you got f grade")
+# else:
+#     print("no babe")
+# chang_name = int(input("enter your chang number\n"))
+# if chang_name != 30:
+#     print("your win")
+# else:
+#     print("sorry")
+# Password = str(input("type the password\n"))
+# correct_password = "moa123456"
+# if Password == correct_password:
+#     print("welcome to my phon")
+# else:
+#     print("sorry")
+# typed = str(input( "please typed (yes) , (No) or (maybe)\n"))
+# if typed == "yes":
+#     print(f" you typed {typed}")
+# elif typed == "no":
+#     print(f"you typed {typed}")
+# elif typed == "maybe":
+#     print(f"you typed {typed}")
+# else:
+#     print(f"you typed {typed}, which is not an option \nplease stick to the options")
+# number = int(input("enter number\n"))
+# correct_number = 55
+# if number== correct_number:
+#      print("very god ")
+# else:
+#     print("sorry")
+# area = input("chose the (cairo), (alex) or (giza)\n")
+# if area.lower() =="cairo":
+#     print("you chose cairo")
+# elif area.upper()== "alex":
+#     print("you chose alex")
+# elif area.lower()== "giza":
+#     print("you chose giza")
+# else:
+#     print("sorry you can,t chose this area")
+# area = input("chose the (cairo), (alex) or (giza)\n")
+# if area.lower()== "cairo" or area.lower()== "alex" or area.lower()== "giza":
+#    #print(f"you chose {area}")
+# age = int(input("how old are you\n"))
+# cared = str(input ("do you have card\n"))
+# if age >=15 and cared =="yes":
+#     print("you the can drive")
+# elif age <=15 and cared =="no":
+#     print("sorry you can,t drive")
+# else:
+# #     print(f"sorry your enter of {cared} is not understood")
+# is_egyptian = (input("Are you egyptian?\n"))
+# if is_egyptian.lower()== "yes":
+#     print("good that is first step")
+#     is_18 = input("Are you above 18?\n")
+#     if is_18.lower()=="yes":
+#         print("you can have on ID card")
+#     else:
+#         print("sorry you can,t have ID card")
+# else:
+#     print("sorry, on egyptian ID is give only to egyptian people")
